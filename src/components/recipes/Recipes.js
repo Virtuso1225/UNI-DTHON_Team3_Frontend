b5f16e1d40dbb17a98d3febce80dca35cfdd2b3f
Virtuso1225 <dominico1225@gmail.com>
2021-11-07 03:34:36 +0900

    Recipes page --Spinner add

--- src/components/recipes/Recipes.js
+++ src/components/recipes/Recipes.js
@@ -1,18 +1,20 @@
 import axios from 'axios';
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { ScrollView, View, StyleSheet, Linking } from 'react-native';
-import { useEffect } from 'react/cjs/react.development';
-import { ListContext } from '../../context/List';
 import url from '../../Global';
 import RecipeCard from '../card/RecipeCard';
+import { ListContext, ProgressContext } from '../../context';
 import {
   BackgroundWrapper,
   BodySection,
   HeaderSection,
   PageHeader,
 } from '../menu/MenuStyle';
+import Spinner from '../..';
 
 const Recipes = () => {
+  const { spinner, inProgress } = useContext(ProgressContext);
+
   const dataurl = url();
   const getList = async () => {
     try {
@@ -24,37 +26,41 @@ const Recipes = () => {
 
   const { menuList, setMenuList } = useContext(ListContext);
 
-  const ListFatch = async () => {
-    const list = await getList();
-    setMenuList(list.data.body);
+  const ListFetch = async () => {
+    try {
+      spinner.start();
+      const list = await getList();
+      setMenuList(list.data.body);
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setTimeout(spinner.stop, 2000);
+    }
   };
 
   useEffect(() => {
-    ListFatch();
+    ListFetch();
   }, []);
 
   return (
-    <ListContext.Consumer>
-      {({ menuList }) => (
-        <BackgroundWrapper>
-          <HeaderSection>
-            <PageHeader>레시피 탐색</PageHeader>
-          </HeaderSection>
-          <BodySection>
-            <ScrollView contentContainerStyle={styles.contentContainer}>
-              {menuList.map((content) => (
-                <RecipeCard
-                  key={content._id}
-                  title={content.name}
-                  subTitle={content.subname}
-                  
-                />
-              ))}
-            </ScrollView>
-          </BodySection>
-        </BackgroundWrapper>
-      )}
-    </ListContext.Consumer>
+    <BackgroundWrapper>
+      {inProgress ? <Spinner /> : <></>}
+
+      <HeaderSection>
+        <PageHeader>레시피 탐색</PageHeader>
+      </HeaderSection>
+      <BodySection>
+        <ScrollView contentContainerStyle={styles.contentContainer}>
+          {menuList.map((content) => (
+            <RecipeCard
+              key={content._id}
+              title={content.name}
+              subTitle={content.subname}
+            />
+          ))}
+        </ScrollView>
+      </BodySection>
+    </BackgroundWrapper>
   );
 };
 
